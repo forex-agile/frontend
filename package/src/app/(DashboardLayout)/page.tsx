4b9ee6fc5e44a7ed90bb3eb600875cff1f5e5020
TommyNg <siuwang555@gmail.com>
2024-09-04 10:11:38 +0800

    Update: Path for Portfolio

--- package/src/app/(DashboardLayout)/page.tsx
+++ package/src/app/(DashboardLayout)/page.tsx
@@ -3,7 +3,7 @@ import { Grid, Box } from '@mui/material';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
 // components
 import SalesOverview from '@/app/(DashboardLayout)/components/dashboard/SalesOverview';
-import YearlyBreakup from '@/app/(DashboardLayout)/components/dashboard/YearlyBreakup';
+import PortfolioCard from '@/app/(DashboardLayout)/components/dashboard/PortfolioCard';
 import RecentTransactions from '@/app/(DashboardLayout)/components/dashboard/RecentTransactions';
 import ProductPerformance from '@/app/(DashboardLayout)/components/dashboard/ProductPerformance';
 import Blog from '@/app/(DashboardLayout)/components/dashboard/Blog';
@@ -20,7 +20,7 @@ const Dashboard = () => {
           <Grid item xs={12} lg={4}>
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                <YearlyBreakup />
+                <PortfolioCard />
               </Grid>
               <Grid item xs={12}>
                 <MonthlyEarnings />
