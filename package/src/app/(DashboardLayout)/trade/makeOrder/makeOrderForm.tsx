7e32a21984526d045dc43567b5fbb042b4966214
TommyNg <siuwang555@gmail.com>
2024-09-10 17:41:06 +0800

    Revamp: Order Form overall layout

--- package/src/app/(DashboardLayout)/trade/makeOrder/makeOrderForm.tsx
+++ package/src/app/(DashboardLayout)/trade/makeOrder/makeOrderForm.tsx
@@ -1,37 +1,112 @@
 import React, { useState } from 'react';
 import { SelectChangeEvent } from '@mui/material/Select';
-import { Typography, Box, Table, TableHead, TableRow, TableCell, TableBody, Select, MenuItem, TextField, Button, Grid } from '@mui/material';
+import { Typography, Box, Table, TableHead, TableRow, TableCell, TableBody, Select, MenuItem, TextField, Button, Grid, Stack } from '@mui/material';
 import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
 import { Widgets } from '@mui/icons-material';
 
+// Date Picker
+import dayjs, { Dayjs } from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
 const MakeOrderForm: React.FC = () => {
-    const [fromCurrency, setFromCurrency] = useState('');
-    const [toCurrency, setToCurrency] = useState('');
-    const [amount, setAmount] = useState('');
-    const [limitPrice, setLimitPrice] = useState('');
 
-    const handleLimitPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setLimitPrice(event.target.value);
+    // Variables for both Market and Limit order form 
+    const [orderType, setOrderType] = useState<'market' | 'limit'>('market'); // Default to Market order
+    const [orderSide, setOrderSide] = useState<'buy' | 'sell'>('buy');        // Default to Buy order
+    const [baseFxCurrencyCode, setBaseFxCurrencyCode] = useState('');
+    const [quoteFxCurrencyCode, setQuoteFxCurrencyCode] = useState('');
+    const [total, setTotal] = useState('');
+    const [expirationDate, setExpirationDate] = useState<Dayjs | null>(dayjs());
+
+
+    // Variables for only Limit order form
+    const [limit, setLimit] = useState('');
+
+    // Handlers for both Market and Limit order form
+    const handleBaseFxCurrencyCode = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setBaseFxCurrencyCode(event.target.value);
     };
 
-    const handleFromCurrencyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFromCurrency(event.target.value);
+    const handleQuoteFxCurrencyCode = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuoteFxCurrencyCode(event.target.value);
     };
 
-    const handleToCurrencyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setToCurrency(event.target.value);
+    const handleTotalChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTotal(event.target.value);
     };
 
-    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAmount(event.target.value);
+    const handleLimitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLimit(event.target.value);
     };
 
-    const handleSubmit = () => {
-        // Handle submit logic here
-    };
+    // const handleExpirationDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setExpirationDate(event.target.value);
+    // };
 
-    const [orderType, setOrderType] = useState<'market' | 'limit'>('market');
 
+    const apiDomain = 'http://localhost:8080';
+
+    // TODO: Add authorization token in the header
+    // Handler for submitting the Market form
+    const handleMarketOrderSubmit = () => {
+        fetch(`${apiDomain}/api/v1/orders`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                baseFxCurrencyCode: baseFxCurrencyCode,
+                quoteFxCurrencyCode: quoteFxCurrencyCode,
+                total: total,
+                orderType: orderType,
+                orderSide: orderSide,
+                expirationDate: expirationDate ? expirationDate.toISOString() : null //Convert current date to ISO 8601 format
+            })
+
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+    }
+
+    // Handler for submitting the Limit form
+    const handleLimitOrderSubmit = () => {
+        fetch(`${apiDomain}/api/v1/orders`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                baseFxCurrencyCode: baseFxCurrencyCode,
+                quoteFxCurrencyCode: quoteFxCurrencyCode,
+                total: total,
+                limit: limit,
+                orderType: orderType,
+                orderSide: orderSide,
+                expirationDate: expirationDate ? expirationDate.toISOString() : null //Convert current date to ISO 8601 format
+            })
+
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+
+
+    // Handlers for changing the order type and side via Market & Limit buttons
     const handleOrderTypeChange = (type: 'market' | 'limit') => {
         setOrderType(type);
     };
@@ -44,29 +119,55 @@ const MakeOrderForm: React.FC = () => {
         handleOrderTypeChange('limit');
     };
 
+    // Handlers for changing the order side via Buy & Sell buttons
+    const handleOrderSideChange = (side: 'buy' | 'sell') => {
+        setOrderSide(side);
+    };
+
+    const handleBuyOrderClick = () => {
+        handleOrderSideChange('buy');
+    };
+
+    const handleSellOrderClick = () => {
+        handleOrderSideChange('sell');
+    };
+
     return (
         <DashboardCard title='Spot Order'>
             <>
-                <Button variant="outlined" onClick={handleMarketOrderClick} style={{ backgroundColor: orderType === 'market' ? 'black' : 'white', color: orderType === 'market' ? 'white' : 'black' }}>
-                    Market Order
-                </Button>
-                <Button variant="outlined" onClick={handleLimitOrderClick} style={{ backgroundColor: orderType === 'limit' ? 'black' : 'white', color: orderType === 'limit' ? 'white' : 'black' }}>
-                    Limit Order
-                </Button>
+                <Grid>
+                    <Button variant="outlined" onClick={handleMarketOrderClick} style={{ backgroundColor: orderType === 'market' ? 'black' : 'white', color: orderType === 'market' ? 'white' : 'black' }}>
+                        Market
+                    </Button>
+                    <Button variant="outlined" onClick={handleLimitOrderClick} style={{ backgroundColor: orderType === 'limit' ? 'black' : 'white', color: orderType === 'limit' ? 'white' : 'black' }}>
+                        Limit
+                    </Button>
+                </Grid>
 
+                <Grid mt={1}>
+                    <Button variant="outlined" onClick={handleBuyOrderClick} style={{ backgroundColor: orderSide === 'buy' ? 'black' : 'white', color: orderSide === 'buy' ? 'white' : 'black' }}>
+                        Buy
+                    </Button>
+                    <Button variant="outlined" onClick={handleSellOrderClick} style={{ backgroundColor: orderSide === 'sell' ? 'black' : 'white', color: orderSide === 'sell' ? 'white' : 'black' }}>
+                        Sell
+                    </Button>
+                </Grid>
+
+                {/* TODO: Dropdown menu for currency */}
                 <Grid container spacing={2} mt={2}>
                     {orderType === 'market' ? (
+
+                        // Market Order Form
                         <>
                             <Grid item>
-                                <Typography variant="subtitle1" sx={{ marginBottom: '1rem' }}>From Currency</Typography>
-                                <TextField value={fromCurrency} onChange={handleFromCurrencyChange} fullWidth />
+                                <TextField
+                                    label={orderSide === 'buy' ? 'Buy Currency' : 'Sell Currency'}
+                                    value={baseFxCurrencyCode}
+                                    onChange={handleBaseFxCurrencyCode}
+                                    fullWidth placeholder='Quote Fx' />
                             </Grid>
+
                             <Grid item>
-                                <Typography variant="subtitle1" sx={{ marginBottom: '1rem' }}>To Currency</Typography>
-                                <TextField value={toCurrency} onChange={handleToCurrencyChange} fullWidth />
-                            </Grid>
-                            <Grid item>
-                                <Typography variant="subtitle1" sx={{ marginBottom: '1rem' }}>Amount</Typography>
                                 <TextField
                                     label="Amount"
                                     autoFocus
@@ -74,42 +175,105 @@ const MakeOrderForm: React.FC = () => {
                                     id="name"
                                     name="Currency"
                                     type="number"
-                                    value={amount}
-                                    placeholder="Amount"
-                                    onChange={handleAmountChange}
+                                    value={total}
+                                    placeholder="Total"
+                                    onChange={handleTotalChange}
                                     fullWidth
                                 />
                             </Grid>
+                            <Grid item>
+                                <TextField
+                                    label="Settlement Currency"
+                                    value={quoteFxCurrencyCode}
+                                    onChange={handleQuoteFxCurrencyCode}
+                                    fullWidth placeholder="Base Fx" />
+                            </Grid>
+                            <Grid item>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DateTimePicker
+                                        label="Expiry Date"
+                                        value={expirationDate}
+                                        onChange={(newDate) => setExpirationDate(newDate)}
+                                    />
+                                </LocalizationProvider>
+                            </Grid>
                         </>
                     ) : (
+                        // Limit Order Form
                         <>
                             <Grid item>
-                                <Typography variant="subtitle1" sx={{ marginBottom: '1rem' }}>From Currency</Typography>
-                                <TextField value={fromCurrency} onChange={handleFromCurrencyChange} fullWidth />
+                                <TextField
+                                    label={orderSide === 'buy' ? 'Buy Currency' : 'Sell Currency'}
+                                    value={baseFxCurrencyCode}
+                                    onChange={handleBaseFxCurrencyCode}
+                                    fullWidth placeholder="Base Fx" />
+                            </Grid>
+
+                            <Grid item>
+                                <TextField
+                                    label="Amount"
+                                    value={total}
+                                    type='number'
+                                    onChange={handleTotalChange}
+                                    fullWidth placeholder="Total" />
+                            </Grid>
+
+                            <Grid item>
+                                <TextField
+                                    label="Settlement Currency"
+                                    value={quoteFxCurrencyCode}
+                                    onChange={handleQuoteFxCurrencyCode}
+                                    fullWidth placeholder="Quote Fx" />
                             </Grid>
                             <Grid item>
-                                <Typography variant="subtitle1" sx={{ marginBottom: '1rem' }}>To Currency</Typography>
-                                <TextField value={toCurrency} onChange={handleToCurrencyChange} fullWidth />
+                                <TextField
+                                    label={orderSide === 'buy' ?
+                                        <>
+                                            <strong style={{ color: 'red' }}>Max </strong>
+                                            Settlement
+                                        </>
+                                        :
+                                        <strong style={{ color: 'red' }}>Min </strong>}
+                                    value={limit}
+                                    type='number'
+                                    onChange={handleLimitChange}
+                                    fullWidth placeholder="Limit" />
                             </Grid>
+
+
+                            {/* TODO: DateTime picker */}
                             <Grid item>
-                                <Typography variant="subtitle1" sx={{ marginBottom: '1rem' }}>Amount</Typography>
-                                <TextField value={amount} onChange={handleAmountChange} fullWidth />
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DateTimePicker
+                                        label="Expiry Date"
+                                        value={expirationDate}
+                                        onChange={(newDate) => setExpirationDate(newDate)}
+                                    />
+                                </LocalizationProvider>
                             </Grid>
+
+                            {/* TODO: FX rates */}
                             <Grid item>
-                                <Typography variant="subtitle1" sx={{ marginBottom: '1rem' }}>Limit Price</Typography>
-                                <TextField value={limitPrice} onChange={handleLimitPriceChange} fullWidth />
+                                {/* <TextField value={ } onChange={ } fullWidth /> */}
                             </Grid>
+
+
+
                         </>
                     )}
                 </Grid>
 
                 <Grid container justifyContent="flex-end" mt={2}>
-                    <Button variant="contained" color="primary" onClick={handleSubmit}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={orderType === 'market' ? handleMarketOrderSubmit : handleLimitOrderSubmit}
+                    >
                         Submit
                     </Button>
                 </Grid>
             </>
-        </DashboardCard>
+        </DashboardCard >
     );
 }
 
