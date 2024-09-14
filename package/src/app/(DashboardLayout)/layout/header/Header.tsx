52cc54e571d8472c86ee22d9f6b937a10d0a99e1
TommyNg <siuwang555@gmail.com>
2024-09-14 18:41:16 +0800

    ADD: dropdown menu fetching for BaseCurrency Converter

--- package/src/app/(DashboardLayout)/layout/header/Header.tsx
+++ package/src/app/(DashboardLayout)/layout/header/Header.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button, Select, MenuItem, Typography } from '@mui/material';
+import React, { use, useEffect, useState } from 'react';
+import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button, Select, MenuItem, Typography, Autocomplete, PaperProps, Paper, TextField } from '@mui/material';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 // components
@@ -15,6 +15,7 @@ interface ItemType {
 const Header = ({ toggleMobileSidebar }: ItemType) => {
   const { currency, setCurrency } = useCurrencyContext();
   const [baseCurrency, setBaseCurrency] = useState(currency);
+  const [currencyOptions, setCurrencyOptions] = useState<string[]>([]);
 
   const user = localStorage.getItem('user');
   const parsedUser = user ? JSON.parse(user) : null;
@@ -22,37 +23,57 @@ const Header = ({ toggleMobileSidebar }: ItemType) => {
   const userId = parsedUser && parsedUser.id ? parsedUser.id : null;
   const userName = parsedUser && parsedUser.username ? parsedUser.username : null;
 
-  const handleCurrencyChange = (event: SelectChangeEvent<string>) => {
-    const newCurrency = event.target.value;
-    setBaseCurrency(newCurrency);
-    setCurrency(newCurrency);
+  const CurrencyDropDownMenu: React.FC<PaperProps> = (props) => (
+    <Paper {...props} sx={{ maxHeight: 150, scrollbarWidth: 'none' }} />
+  );
 
-    // // Call post method to pass the updated baseCurrency to the mock API
-    // // replace 'YOUR_API_ENDPOINT' with the actual API endpoint
-    // fetch('api/change/base-currency', {
-    //   method: 'POST',
-    //   body: JSON.stringify({ currency: newCurrency }),
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log('Success:', data.message);
-    //   })
-    //   .catch((error) => {
-    //     // Handle any errors
-    //   });
+  const handleCurrencyChange = (event: React.SyntheticEvent<Element, Event>, value: string | null) => {
+    if (value) {
+      setBaseCurrency(value);
+      setCurrency(value);
+    }
   };
+
+  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+  useEffect(() => {
+    fetch(`${baseURL}/api/v1/currency`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}` //
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+
+        // format the data to match the fxRate state
+        const formattedData = data.map((currency: { currencyCode: string; currencyName: string; }) => {
+          return {
+            currencyCode: currency.currencyCode,
+          }
+        });
+
+        console.log('formattedData:', formattedData);
+        const currencyCodes = formattedData.map((currency: { currencyCode: String; }) => currency.currencyCode);
+        setCurrencyOptions(currencyCodes);
+        // setCurrencyOptions(formattedData);
+
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [currency]);
+
 
   const handleLogout = () => {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
+    localStorage.removeItem('currency');
     window.location.href = '/authentication/login';
   };
 
-  // TODO: Call api to get the possible currency options
-  const currencyOptions = ['USD', 'EUR', 'GBP', 'JPY', 'HKD', 'CAD', 'AUD', 'NZD']; // Add more currencies as needed
 
   const AppBarStyled = styled(AppBar)(({ theme }) => ({
     boxShadow: 'none',
@@ -106,13 +127,16 @@ const Header = ({ toggleMobileSidebar }: ItemType) => {
         <Box flexGrow={1} />
 
         <Stack spacing={1} direction="row" alignItems="center">
-          <Select value={baseCurrency} onChange={handleCurrencyChange}>
-            {currencyOptions.map((option) => (
-              <MenuItem key={option} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
+          <Autocomplete
+            disablePortal
+            options={currencyOptions}
+            sx={{ width: 150 }}
+            value={baseCurrency}
+            onChange={handleCurrencyChange}
+            isOptionEqualToValue={(option, value) => option === value}
+            renderInput={(params) => <TextField {...params} />}
+            PaperComponent={CurrencyDropDownMenu}
+          />
 
           {isLoggedIn ? (
             <Button variant="outlined" component={Link} href="" onClick={handleLogout} disableElevation color="primary">
